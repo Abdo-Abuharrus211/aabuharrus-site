871f14d55616da46c694543769ff7952ab6e9b1f
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-06-12 15:07:04 -0700

    Define the Projects and Skills collections' schemas

--- mechanical-metal/src/content.config.ts.ts
+++ mechanical-metal/src/content.config.ts.ts
@@ -3,22 +3,37 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders'; 
+import { glob, file } from 'astro/loaders';
 
 /* TODO: Will likely use the `file` loader since most of the data will live in a single JSON file.
     "Use this loader when your data file can be parsed as an array of objects."
 */
 
 // 3. Define your collection(s)
-const projects = defineCollection({ 
+const projects = defineCollection({
     loader: file("src/data/projects.json"),
     schema: z.object({
         // TODO: define the schemas
+        name: z.string(),
+        imagePath: z.string(),
+        date: z.date(),
+        text: z.string(),
+        keyPoints: z.array(z.string()), //array of string for the bullet points
+        link: z.string(),
+        status: z.enum(["Planning", "In Progress", "Complete"]),
+        tags: z.array(z.string()),
     })
 });
 
-const skills = defineCollection({ /* ... */ });
+const skills = defineCollection({
+    loader: file("src/data/skills.json"),
+    schema: z.object({
+        name: z.string(),
+        icon: z.string(),
+        points: z.array(z.string()),
+    })
+});
 // const hobbies = defineCollection({/*..*/});
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { projects, skills};
+export const collections = { projects, skills };
