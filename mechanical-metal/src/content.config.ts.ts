2b9e0eff3e2039cfb944b89d42a6a581ec7c05c1
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-06-12 13:54:26 -0700

    Set up a file loader example

--- mechanical-metal/src/content.config.ts.ts
+++ mechanical-metal/src/content.config.ts.ts
@@ -1,11 +1,22 @@
+// For more about this check out https://docs.astro.build/en/guides/content-collections/
+
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
-
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+import { glob, file } from 'astro/loaders'; 
+
+/* TODO: Will likely use the `file` loader since most of the data will live in a single JSON file.
+    "Use this loader when your data file can be parsed as an array of objects."
+*/
 
 // 3. Define your collection(s)
-const projects = defineCollection({ /* ... */ });
+const projects = defineCollection({ 
+    loader: file("src/data/projects.json"),
+    schema: z.object({
+        // TODO: define the schemas
+    })
+});
+
 const skills = defineCollection({ /* ... */ });
 // const hobbies = defineCollection({/*..*/});
 
