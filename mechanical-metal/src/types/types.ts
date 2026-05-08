6973081b712acae7a8b13368c566a6476f3e755b
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-05-07 21:03:34 -0700

    Create a types file for TS

---
+++ mechanical-metal/src/types/types.ts
@@ -0,0 +1,24 @@
+
+
+export type Project = {
+    id: number;
+    name: string;
+    teaser: string;
+    featured: boolean;
+    images: string[];
+    date: string;
+    text: string;
+    keyPoints: string[];
+    status: string;
+    link: string;
+    github: string;
+    tags: string[];
+};
+
+
+export type Skill = {
+    id: number,
+    name: string,
+    icon: string,
+    points: string[]
+};
