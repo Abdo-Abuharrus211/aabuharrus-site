545fb65302ee3294d13d21f85580747191399f6b
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-05-07 21:12:46 -0700

    Implement Svelte data store for project Modals -  different components sub to this for state

---
+++ mechanical-metal/src/stores/projModalStore.ts
@@ -0,0 +1,17 @@
+import { writable } from "svelte/store";
+import type { Project } from "../types/types"
+
+export const selectedProject = writable<Project | null>(null);
+export const showModal = writable(false);
+
+export function openModal(proj: Project) {
+    showModal.set(true);
+    selectedProject.set(proj);
+}
+
+export function closeModal() {
+    showModal.set(false);
+    selectedProject.set(null);
+
+}
+
