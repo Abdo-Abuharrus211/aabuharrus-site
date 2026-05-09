a1b8ee0f5bfae27529b65112d28715153474882d
abdu <aabuharrus@gmail.com>
2026-05-08 17:54:55 -0700

    Implement tag selection for the chips in the filter bar

--- mechanical-metal/src/components/projectGallery.svelte
+++ mechanical-metal/src/components/projectGallery.svelte
@@ -15,33 +15,60 @@
       new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
   );
 
-  // map with tags as keys array of IDs as value
-  const allTags = new Map<string, number[]>();
-  // mapped projectsd
-  const pMap = new Map<number, Project>();
-  projects.forEach((p: Project) => {
-    if (!pMap.has(p.id)) pMap.set(p.id, p);
-  });
-
-  const selectedToggles: string[] = [];
   /**
    * TODO:
-   * Click handle for the toggle buttons
    * filtering for tags
    * rendering based on IDs that have the tags...
    *
    */
+
+  // mapped projects
+  const pMap = new Map<number, Project>();
+  projects.forEach((p) => {
+    if (!pMap.has(p.id)) pMap.set(p.id, p.data);
+  });
+
+  console.log(pMap);
+
+  // map with tags as keys array of IDs as value
+  const allTags = new Map<string, number[]>();
+  // Extract all unique tags
+  projects.forEach((p: Project) => {
+    if (p && p.data.tags && Array.isArray(p.data.tags)) {
+      p.data.tags.forEach((tag: string) => {
+        if (allTags.has(tag)) {
+          let cur: number[] = allTags.get(tag);
+          cur.push(p.id);
+          allTags.set(tag, cur);
+        } else {
+          allTags.set(tag, [p.id]);
+        }
+      });
+    }
+  });
+
+  const selectedToggles: Set<string> = new Set();
 </script>
 
 <div class="margin5">
+  <!-- filter bar -->
   <div id="filter-box" class="container center">
     <div id="filter-bar">
       <h3 class="code-text">Filter</h3>
       {#each tags as t}
-        <button class="chip-btn">{t}</button>
+        <button
+          class="chip-btn"
+          onclick={() => {
+            // TODO: add the selected class for styling
+            selectedToggles.has(t)
+              ? selectedToggles.delete(t)
+              : selectedToggles.add(t);
+          }}>{t}</button
+        >
       {/each}
     </div>
   </div>
+  <!-- Gallery -->
   <div class="proj-gallery margin4">
     {#each sortedProjects as project}
       <ProjectCard
