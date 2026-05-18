ab860f2d9606aea8c83fb88114941719ea574c0e
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-05-18 13:47:24 -0700

    Add some scaffolding for

--- mechanical-metal/src/components/about.svelte
+++ mechanical-metal/src/components/about.svelte
@@ -5,7 +5,7 @@
    * Render ONLY the content for the current toggle
    */
   const toggles: string[] = ["Who's Abdu?", "Background", "Interests/Hobbies"];
-  const content = new Map<string, string>();
+  const content = new Map<string, string>(); // placeholder map, toggle:contenObject
   let currentToggle = $state<string>("");
 
   content.set(toggles[0], "This is all about me and me and me and meeee");
@@ -29,35 +29,39 @@
    * Switch the content displayed, triggering Svelte reactivity
    */
   function switchContent() {}
-
-
 </script>
 
-<div class="container center">
-  <div id="toggle-bar margin3">
+<div class="container">
+  <div id="toggle-bar" class="margin3 center">
     {#each toggles as tog}
       <button
         class="chip-btn"
-        onclick={() => setToggle(tog)}
         class:active-chip={currentToggle === tog}
+        onclick={() => setToggle(tog)}
       >
         {tog}
       </button>
-    {/each}
+      {/each}
+    </div>
 
     <!-- Content -->
     <div id="content-box margin3 padding2">
       <div class="content-img margin3">
         <img src="https://picsum.photos/1200/720" alt="placeholder-img" />
+        <small>Img Caption Here...</small>
       </div>
       <div>
+      <h3>Heading Here</h3>
         <p>{content?.get(currentToggle)}</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In molestias
+          eos ducimus architecto asperiores excepturi ad ratione adipisci
+          voluptatem dolore tempora quo, et odio quaerat amet magnam expedita
+          dolorem modi!
+        </p>
       </div>
     </div>
-  </div>
 </div>
 
-
 <style>
-
 </style>
