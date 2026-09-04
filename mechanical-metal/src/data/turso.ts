020948be592f574a5208a89801b3372e3dfe13fb
abdu <aabuharrus@gmail.com>
2026-09-03 18:50:29 -0700

    adjust token

--- mechanical-metal/src/data/turso.ts
+++ mechanical-metal/src/data/turso.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
 const url = import.meta.env.TURSO_DATABASE_URL;
-const authToken = import.meta.env.TURSO_AUTH_TOKEN_READONLY;
+const authToken = import.meta.env.TURSO_AUTH_TOKEN;
 
 export const turso = url ? createClient({ url, authToken }) : null;
