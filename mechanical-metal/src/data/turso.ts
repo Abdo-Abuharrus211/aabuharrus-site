dc6ff5df6d4e1c02d28a69dec58f7c6c30b08c4d
abdu <aabuharrus@gmail.com>
2026-09-03 19:13:38 -0700

    Remove tokent vars - no tokens to speak of

--- mechanical-metal/src/data/turso.ts
+++ mechanical-metal/src/data/turso.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@libsql/client";
 
-const url = import.meta.env.TURSO_DATABASE_URL;
-const authToken = import.meta.env.TURSO_AUTH_TOKEN;
-
-export const turso = url ? createClient({ url, authToken }) : null;
+export const turso = createClient({
+  url: "file:./src/data/portfolio.db",
+});
