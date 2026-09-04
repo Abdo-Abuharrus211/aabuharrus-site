441d544ba5a7e3b06bf4f25f27a23196e338c58f
abdu <aabuharrus@gmail.com>
2026-09-03 19:01:37 -0700

    Adjust which client imported

--- mechanical-metal/src/data/turso.ts
+++ mechanical-metal/src/data/turso.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 
 const url = import.meta.env.TURSO_DATABASE_URL;
 const authToken = import.meta.env.TURSO_AUTH_TOKEN;
